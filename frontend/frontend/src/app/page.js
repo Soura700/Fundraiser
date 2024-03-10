66ac16597a82e637345be7c784f1515f26e2303b
Soura700 <108584756+Soura700@users.noreply.github.com>
2024-03-10 19:45:54 +0530

    Structure Creation

--- frontend/frontend/src/app/page.js
+++ frontend/frontend/src/app/page.js
@@ -3,10 +3,6 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      Hello World
-      <br/>
-      Sattwik magi
-    </main>
+    <div>Home page</div>
   );
 }
