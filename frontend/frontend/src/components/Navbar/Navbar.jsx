68146ff270a292cc57deb267a128b81b731de21b
Soura700 <108584756+Soura700@users.noreply.github.com>
2024-03-18 09:55:40 +0530

    Navbar Done

--- frontend/frontend/src/components/Navbar/Navbar.jsx
+++ frontend/frontend/src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import Image from 'next/image'
 // import { Link, useNavigate } from 'react-router-dom';
 
 // import { useStateContext } from '../context';
-// import { CustomButton } from './';
 
 
 import search from "@/assets/search.svg";
 import menu from "@/assets/menu.svg";
 import thirdweb from "@/assets/thirdweb.png";
 import { navlinks } from '@/constants';
+import CustomButton from '../CustomButton/Custombutton';
 
 
 const Navbar = () => {
@@ -20,10 +20,11 @@ const Navbar = () => {
   const [toggleDrawer, setToggleDrawer] = useState(false);
 //   const { connect, address } = useStateContext();
 
+  const address = 'Create a campaign'
 
 
   return (
-    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-5">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
         
@@ -33,7 +34,7 @@ const Navbar = () => {
       </div>
 
       <div className="sm:flex hidden flex-row justify-end gap-4">
-        {/* <CustomButton 
+        <CustomButton 
           btnType="button"
           title={address ? 'Create a campaign' : 'Connect'}
           styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
@@ -41,7 +42,7 @@ const Navbar = () => {
             if(address) navigate('create-campaign')
             else connect()
           }}
-        /> */}
+        />
 
         {/* <Link to="/profile"> */}
           <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
@@ -88,7 +89,7 @@ const Navbar = () => {
             </ul>
 
             <div className="flex mx-4">
-            {/* <CustomButton 
+            <CustomButton 
               btnType="button"
               title={address ? 'Create a campaign' : 'Connect'}
               styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
@@ -96,7 +97,7 @@ const Navbar = () => {
                 if(address) navigate('create-campaign')
                 else connect();
               }}
-            /> */}
+            />
             </div>
           </div>
         </div>
--- frontend/frontend/src/layout.jsx
+++ frontend/frontend/src/layout.jsx
@@ -1,16 +1,17 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
-import styles from "@/styles/sidebar.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <div className={styles.layout}>
-        <Sidebar />
-        <Navbar />
-        <main className={styles.layout__main__content}>{children}</main>
+    <div className="relative sm:-8 p-4 bg-[#1B1A55] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar/>
       </div>
-    </>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+      </div>
+    </div>
   );
 };
 
